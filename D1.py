ad5f61108c7e38e853c53d6bba5dd27cf8713f17
Subhash Kumar yadav <141063498+SkyPy777@users.noreply.github.com>
2023-08-01 11:18:01 +0530

    Add files via upload

---
+++ D1.py
@@ -0,0 +1,109 @@
+#print "hello world"
+message="Hello world"
+print(message)
+
+#variables
+variable_name = "value"
+Text="Hi,started learning the python!!"
+print (Text)
+
+message1 = "This is the variables"
+message2 = "This is also the variables"
+print(message1)
+print(message2)
+
+my_name = "john"
+my_list = [1,2,3,4]
+
+greeting = 'hello '
+my_name = 'Austin'
+print(greeting+my_name) 
+
+
+
+greeting = "hello"
+my_name = "Austin"
+print(greeting + " " + my_name)
+
+#Data types 
+
+#integer
+num = 2 + 1
+print(num)
+
+add = 10+20
+print(add)
+
+#float
+num = 0.1 + 0.1
+print(num)
+
+num = 3 * 0.1
+print(num)
+
+#string
+name1 = "This is a string."
+name2 = 'This is also a string.'
+print(name1+ '\n' + name2)
+
+#boolean
+is_valid = True
+print(is_valid)
+
+is_invalid = False
+print(is_invalid)
+
+#complex
+z = complex(2, -3)
+print(z)
+
+z = complex(1)
+print(z)
+
+z = complex()
+print(z)
+
+z = complex('5-9j')
+print(z)
+
+#text type
+name = "John"
+print(name) 
+
+#list type
+my_list = ["Jessa", "Kelly", 20, 35.75]
+print(my_list)
+
+my_list2 = list(["Jessa", "Kelly", 20, 35.75])
+print(my_list2)
+
+#tuples
+my_tuple = (11, 24, 56, 88, 78)
+print(my_tuple)
+
+my_tuple2 = tuple((10, 20, 30, 40))
+print(my_tuple2)
+
+#dicratory
+my_dict = {1: "Smith", 2: "Emma", 3: "Jessa"}
+print(my_dict)
+
+my_dict[1] = "Kelly"
+print(my_dict[1])  
+
+name = 'sky'
+age = 21
+height = 1.75
+student = True
+print("name:", name)
+print("age:", age)
+print("height:", height)
+print("student:", student)
+
+
+
+
+
+
+
+
---
+++ D2.py
@@ -0,0 +1,55 @@
+# Accessing elements
+numbers = [1, 2, 3, 4, 5]
+fruits = ["apple", "banana", "orange"]
+
+print(numbers[0])  
+print(fruits[1])   
+
+# Modifying elements
+
+numbers[2] = 30
+print(numbers)  
+
+# Adding elements
+
+fruits.append("grape")
+print(fruits)  
+
+# Removing elements
+
+fruits.remove("banana")
+print(fruits)  
+
+
+for number in numbers:
+    print(number)
+
+# Finding the length
+print(len(fruits))  
+
+# Empty list
+my_list = []
+
+# List with some items
+numbers = [1, 2, 3, 4, 5]
+fruits = ['apple', 'banana', 'orange']
+mixed_list = [1, 'apple', True, 3.14]
+
+fruits = ['apple', 'banana', 'orange']
+
+print(fruits[0])  
+print(fruits[1])  
+print(fruits[-1])  
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(numbers[2:5])  
+print(numbers[:4])   
+print(numbers[6:])   
+
+fruits = ['apple', 'banana', 'orange']
+
+fruits[1] = 'kiwi'
+print(fruits)  
+
+
