2f0e190e0e70996f7c670362bc1bc74f83960a2b
Subhash Kumar yadav <141063498+SkyPy777@users.noreply.github.com>
2023-08-02 09:32:15 +0530

    Add files via upload

---
+++ Sorting.py
@@ -0,0 +1,92 @@
+#Sorting
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print('\nHere is the orginal list:')
+print(cars)
+
+print('\nHere is the sorted list:')
+print(sorted(cars))
+print(cars)
+
+print('\nHere is the orginal list again:')
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse()
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+length = len(cars)
+print(length)
+
+numbers = ['3','5','3','7','8','0']
+print(numbers)
+length= len(numbers)
+print(length)
+
+places = ['Austraila','South Africa','Japan','China']
+print(places)
+places.sort()
+print(places)
+places = ['Austraila','South Africa','Japan','China']
+print(places)
+places.sort(reverse=True)
+print(places)
+
+places = ['Austraila','South Africa','Japan','China']
+print(places)
+places.reverse()
+print(places)
+
+places = ['Austraila','South Africa','Japan','China']
+print(places)
+places.reverse()
+print(places)
+
+places = ['Austraila','South Africa','Japan','China']
+places.sort()
+print(places)
+
+places = ['Austraila','South Africa','Japan','China']
+places.sort(reverse=True)
+print(places)
+
+Guest = ['Roy', 'John','Rock','Diesel','Alan']
+print(Guest)
+length = len(Guest)
+print(str(length) + ' number of guests are attending dinner ')
+
+languages = ['Hindi', 'English', 'bengali', 'Kannada','Tamil']
+print(languages)
+print(languages[0])
+print(languages[2].title())
+print(languages[3].upper())
+print(languages[-2])
+print(languages[2])
+message= ('my first language is ' + languages[0]+ '.')
+print(message)
+languages[1] = 'sanskrit'
+print(languages)
+languages.append('punjabi')
+print(languages)
+languages.remove('bengali')
+print(languages)
+languages.sort()
+print(languages)
+languages.sort(reverse=True)
+print(languages)
+languages.reverse()
+print(languages)
+length = len(languages)
+print(length)
